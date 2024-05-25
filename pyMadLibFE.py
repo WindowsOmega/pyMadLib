da49cf3755bb4f8123b8420965578ba99a9d8d2c
WindowsOmega <166148619+WindowsOmega@users.noreply.github.com>
2024-05-25 10:00:03 -0400

    Add files via upload

---
+++ pyMadLibFE.py
@@ -0,0 +1,9 @@
+q1 = input("Name a day of the week: ")
+q2 = input("Name a thing: ")
+q3 = input("A name of a person: ")
+q4 = input("Name a shape: ")
+
+print(f"On {q1}, my father distroyed his {q2}. Like WHY?! Than, {q3}, wanted to become a {q4}!")
+
+import time
+time.sleep(100)
